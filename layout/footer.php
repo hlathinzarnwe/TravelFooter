333809e92146282f2e84f162de84828c926c3c58
hla thinzar nwe <hlathinzarnwe2000@gmail.com>
2024-05-12 21:40:34 +0630

    footer commit

---
+++ layout/footer.php
@@ -0,0 +1,240 @@
+<html>
+
+<head>
+    <title>Travel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        
+        .icon-center {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+        }
+
+        /* //.................... */
+        .icon-container {
+            display: flex;
+            justify-content: center;
+            /* align-items: center; */
+            /* height: 100vh; Set height to fill the viewport height */
+        }
+        .myimg{
+            background-image: url('../img/beach2.jpg');
+            background-size: cover;
+            background-position: center;
+        }
+    </style>
+</head>
+
+<body>
+
+    <!-- <choose> -->
+    <div class="fluid-container myimg">
+        <div class="container ">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-center">WHY CHOOSE US?</h2>
+                    <p class="text-center">A BRAND NAME YOU CAN TRUST AND RELY ON</p>
+                </div>
+            </div>
+
+
+            <div class="row g-4">
+                <div class=" col-lg-4 col-md-6">
+                    <div class="property-item rounded overflow-hidden">
+                        <div class="position-relative overflow-hidden">
+                            <div class="d-flex pt-2 icon-container">
+                                <span class="rounded-circle bg-primary text-white p-3">
+                                    <i class="fas fa-suitcase-rolling"></i>
+                                </span>
+                            </div>
+                            <div class="p-4 pb-0">
+                                <h2 class="text-black mb-3">AFFORDABLE PRICE CUARNTEE </h2>
+                            
+                                <P class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Commodi id ducimus fugiat reprehenderit porro cumque labore quo totam.
+                                    Eaque ea deleniti nesciunt distinctio.
+                                    
+                                </P>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="property-item rounded overflow-hidden">
+                        <div class="position-relative overflow-hidden">
+                            <div class="d-flex pt-2 icon-container">
+                                <span class="rounded-circle bg-primary text-white p-3">
+                                    <i class="fas fa-globe-americas"></i></span>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-4 pb-0">
+                        <h2 class="text-black mb-3">WIDE VARIETY OF DESTIONS</h2>
+                        <P class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Commodi id ducimus fugiat reprehenderit porro cumque labore quo totam.
+                            Eaque ea deleniti nesciunt distinctio.
+                            
+                        </P>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="property-item rounded overflow-hidden">
+                        <div class="position-relative overflow-hidden">
+                            <div class="d-flex pt-2 icon-container">
+                                <span class="rounded-circle bg-primary text-white p-3 ">
+                                    <i class="fas fa-thumbs-up"></i></span>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="p-4 pb-0">
+                        <h2 class="text-black mb-3">HIGHLY QUALIFIED SERVICE</h2>
+                        <P class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Commodi id ducimus fugiat reprehenderit porro cumque labore quo totam.
+                            Eaque ea deleniti nesciunt distinctio.
+                            
+                        </P>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    <!-- <choose> -->
+
+    <!-- <testimonials> -->
+    <div class="fluid-container my-4">
+        <div class="container">
+
+            <div class="row g-0 gx-5 align-items-end">
+                <div class="col-md-12 mr-10">
+                    <h2 class="text-center">TESTMONIALS</h2>
+                    <p class="text-center">WHAT OUR CUSTOMER SAY ABOUT US</p>
+                </div>
+            </div>
+
+
+            <div class="row g-4">
+                <div class=" col-lg-4 col-md-6">
+                    <div class="property-item rounded overflow-hidden">
+                        <div class="position-relative overflow-hidden">
+                            <div class="d-flex pt-2 icon-container">
+                                <span class="rounded-circle bg-primary text-white p-3">
+                                    <i class="fas fa-comment-dots"></i></span>
+                            </div>
+                            <!-- </div> -->
+                            <div class="p-4 pb-0">
+
+                                <P>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Commodi id ducimus fugiat reprehenderit porro cumque labore quo totam.
+                            
+                                </P>
+                                <h5 class="text-info mb-3">JOIN SMITH</h5>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="property-item rounded overflow-hidden">
+                        <div class="position-relative overflow-hidden">
+                            <div class="d-flex pt-2 icon-container">
+                                <span class="rounded-circle bg-primary text-white p-3">
+                                    <i class="fas fa-comment-dots"></i></span>
+                            </div>
+
+                            <!-- </div> -->
+                            <!-- </div> -->
+                            <div class="p-4 pb-0">
+
+                                <P>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Commodi id ducimus fugiat reprehenderit porro cumque labore quo totam.
+                                
+                                </P>
+                                <h5 class="text-info mb-3">SUSNHILL</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="property-item rounded overflow-hidden">
+                        <div class="position-relative overflow-hidden">
+                            <div class="d-flex pt-2 icon-container">
+                                <span class="rounded-circle bg-primary text-white p-3">
+                                    <i class="fas fa-comment-dots"></i></span>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="p-4 pb-0">
+
+                        <P>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Commodi id ducimus fugiat reprehenderit porro cumque labore quo totam.
+                        
+                        
+                        </P>
+                        <h5 class="text-info mb-3">DAVID WILLAMS</h5>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    </div>
+
+
+    <!-- <testimonials> -->
+    <!-- <footer> -->
+    <div class="fluid-container bg-primary">
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <p class="text-center my-3">Privacy |Terms of use |Contact Supplies</p>
+                    <p class="text-center">Copyright domainname.com All right Reserved|Designed by:references</p>
+                </div>
+                <div class="d-flex pt-2 icon-center">
+                    <a class="btn btn-outline-light btn-social mx-2" href=""><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-outline-light btn-social mx-2" href=""><i class="fab fa-facebook"></i></a>
+                    <a class="btn btn-outline-light btn-social mx-2" href=""><i class="fab fa-youtube"></i></a>
+                    <a class="btn btn-outline-light btn-social mx-2" href=""><i class="fab fa-linkedin-in"></i></a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- </footer> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+
+
+</body>
+
+</html>
